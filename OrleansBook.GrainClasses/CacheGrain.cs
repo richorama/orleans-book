c223b06977e805b52fcc993f4d6199cab0f01a7b
Richard Astbury <richard.astbury@gmail.com>
2021-03-05 14:06:13 +0000

    add a timer to collect stats

--- OrleansBook.GrainClasses/CacheGrain.cs
+++ OrleansBook.GrainClasses/CacheGrain.cs
@@ -7,6 +7,38 @@ namespace OrleansBook.GrainClasses
 {
   public class CacheGrain<T> : Grain<T>, ICacheGrain<T>
   {
+
+    int totalReads = 0;
+    int totalWrites = 0;
+    int readsInLastMin = 0;
+    int writesInLastMin = 0;
+
+    public override Task OnActivateAsync()
+    {
+      var oneMinute = TimeSpan.FromMinutes(1);
+      this.RegisterTimer(this.ResetStats, null, oneMinute, oneMinute);
+
+      return base.OnActivateAsync();
+    }
+
+    Task ResetStats(object _)
+    {
+      this.totalReads += this.readsInLastMin;
+      this.totalWrites += this.writesInLastMin;
+
+      var key = this.GetPrimaryKeyString();
+
+      Console.WriteLine($"{key} total reads: {this.totalReads}");
+      Console.WriteLine($"{key} total writes: {this.totalWrites}");
+      Console.WriteLine($"{key} reads in last min: {this.readsInLastMin}");
+      Console.WriteLine($"{key} writes in last min: {this.writesInLastMin}");
+
+      this.readsInLastMin = 0;
+      this.writesInLastMin = 0;
+
+      return Task.CompletedTask;
+    }
+
     Task Publish(T oldValue, T newValue)
     {
       var message = new Delta<T>(
@@ -22,6 +54,7 @@ namespace OrleansBook.GrainClasses
 
     public async Task Put(T value)
     {
+      this.writesInLastMin += 1;
       var oldValue = this.State;
       this.State = value;
       await this.WriteStateAsync();
@@ -30,6 +63,7 @@ namespace OrleansBook.GrainClasses
 
     public Task<T> Get()
     {
+      this.readsInLastMin += 1;
       return Task.FromResult(this.State);
     }
 
