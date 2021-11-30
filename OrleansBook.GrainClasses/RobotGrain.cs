9e9665eccc7144406b7efbbaac01835e5d5311b0
Richard Astbury <richard.astbury@gmail.com>
2021-11-30 21:15:00 +0000

    add timer

--- OrleansBook.GrainClasses/RobotGrain.cs
+++ OrleansBook.GrainClasses/RobotGrain.cs
@@ -9,6 +9,8 @@ namespace OrleansBook.GrainClasses
 {
   public class RobotGrain : Grain, IRobotGrain
   {
+    int instructionsEnqueued = 0;
+    int instructionsDequeued = 0;
     ILogger<RobotGrain> logger;
     IPersistentState<RobotState> state;
 
@@ -38,6 +40,7 @@ namespace OrleansBook.GrainClasses
       var key = this.GetPrimaryKeyString();
       this.logger.LogWarning($"{key} adding '{instruction}'");
       this.state.State.Instructions.Enqueue(instruction);
+      this.instructionsEnqueued += 1;
       await this.state.WriteStateAsync();
     }
 
@@ -60,9 +63,30 @@ namespace OrleansBook.GrainClasses
         $"{key} returning '{instruction}'");
 
       await this.Publish(instruction);
-
+      this.instructionsDequeued += 1;
       await this.state.WriteStateAsync();
       return instruction;
+    }
+
+    public override Task OnActivateAsync()
+    {
+      var oneMinute = TimeSpan.FromMinutes(1);
+      this.RegisterTimer(this.ResetStats, null, oneMinute, oneMinute);
+      return base.OnActivateAsync();
+    }
+
+    Task ResetStats(object _)
+    {
+      var key = this.GetPrimaryKeyString();
+
+      Console.WriteLine($"{key} enqueued: {this.instructionsEnqueued}");
+      Console.WriteLine($"{key} dequeued: {this.instructionsDequeued}");
+      Console.WriteLine($"{key} queued: {this.state.State.Instructions.Count}");
+
+      this.instructionsEnqueued = 0;
+      this.instructionsDequeued = 0;
+
+      return Task.CompletedTask;
     }
   }
 }
