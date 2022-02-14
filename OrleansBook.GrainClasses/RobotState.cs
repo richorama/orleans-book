151e3685510b0af751642b34374d86b1bc44368e
Richard Astbury <richard.astbury@gmail.com>
2022-02-14 14:19:56 +0000

    upgrade to net6.0

--- OrleansBook.GrainClasses/RobotState.cs
+++ OrleansBook.GrainClasses/RobotState.cs
@@ -1,10 +1,5 @@
 using System;
 using System.Collections.Generic;
-using System.Threading.Tasks;
-using Microsoft.Extensions.Logging;
-using Orleans;
-using Orleans.Runtime;
-using OrleansBook.GrainInterfaces;
 
 namespace OrleansBook.GrainClasses
 {
